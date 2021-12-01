5154895630a07c2401fb82ce50c024c7f75db067
Raul Limon-Hernandez <rlimonhernandez@wil.yearup.org>
2021-12-01 15:22:09 -0500

    push all current code

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
-import React, { Component } from 'react';
-// eslint-disable-next-line 
-import { Navbar, Container, Nav } from 'react-bootstrap';
-// eslint-disable-next-line 
-import { Route, Routes } from 'react-router-dom'; 
+import React from "react";
+// eslint-disable-next-line
+import { Navbar, Container, Nav } from "react-bootstrap";
+// eslint-disable-next-line
+import { Route, Routes, Router } from "react-router-dom";
+import DepositsComponent from "./components/DepositsComponent";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-class App extends Component { 
-   constructor(props){
-       super(props);
-       this.state = {
-       
-       }
- }
-
-render() { 
-  return ( 
-
-      <div className="App">
-            
-           <header>
-          </header>
-
-          <main>
-          
-          <Routes>
-          </Routes>
-
-          </main>
-
-
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<DepositsComponent />}></Route>
+        </Routes>
       </div>
-    )
-  }
-
+    </div>
+  );
 }
 
-
-
-export default App
+export default App;
---
+++ src/components/DepositsComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const DepositsComponent = () => {
+  return (
+    <div>
+      <h1>Deposit</h1>
+      <hr></hr>
+      <h1> Account Balance:</h1>
+      <h1> Available Balance:</h1>
+      <h1> Amount:</h1>
+      <input type="text" placeholder="Enter amount to deposit"></input>
+      <button className=""></button>
+    </div>
+  );
+};
+
+export default DepositsComponent;
