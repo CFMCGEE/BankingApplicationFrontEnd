257a1f300815a35c63c472861e5e2a5ad66b3f98
Caleb D <89933841+CFMCGEE@users.noreply.github.com>
2021-12-01 11:56:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom'; 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+   constructor(props){
+       super(props);
+       this.state = {
+       
+       }
+ }
+
+render() {
+  return ( 
+
+      <div className="App">
+            
+           <header>
+          </header>
+
+          <main>
+          
+          <Routes>
+          </Routes>
+
+          </main>
+
+
+      </div>
+    )
+  }
+
 }
 
-export default App;
+
+export default App
