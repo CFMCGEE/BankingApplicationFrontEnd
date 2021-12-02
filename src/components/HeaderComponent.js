e982b64109817b3890bb584e1679dfcd1b63a722
Boris <borisgerard333@gmail.com>
2021-12-02 16:20:03 -0500

    added customer to header component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,43 +1,54 @@
-import React from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 const HeaderComponent = () => {
-    return (
-        <div>
-     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink className="navbar-brand" to="/">
-        One Piece of Bread Banking
-                </NavLink>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-                <NavLink className="nav-link" to="/accounts">
-                  Add account
-                </NavLink>
+          One Piece of Bread Banking
+        </NavLink>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <NavLink className="nav-link" to="/accounts">
+                Add account
+              </NavLink>
+            </li>
+            <li class="nav-item active">
+              <NavLink className="nav-link" to="/deposits">
+                Deposit Add
+              </NavLink>
+            </li>
 
-      </li>
-      <li class="nav-item active">
-                <NavLink className="nav-link" to="/deposits">
-                  Deposit Add
-                </NavLink>
-      </li>
+            <li class="nav-item active">
+              <NavLink className="nav-link" to="/create-customer">
+                create customer
+              </NavLink>
+            </li>
 
-<li class="nav-item active">
-                <NavLink className="nav-link" to="/create-customer">
-                  create customer
-                </NavLink>
-      </li>
-    </ul>
-    <span class="navbar-text">
-      Getcho Bread up
-    </span>
-  </div>
-</nav>
+            <li class="nav-item active">
+              <NavLink className="nav-link" to="/customers">
+                List of customers
+              </NavLink>
+            </li>
+          </ul>
+          <span class="navbar-text">Getcho Bread up</span>
         </div>
-    )
-}
+      </nav>
+    </div>
+  );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
