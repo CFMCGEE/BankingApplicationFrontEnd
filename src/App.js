1b177b4c64e621e9ea5d94dbf4fc22dc39cf0667
Boris <borisgerard333@gmail.com>
2021-12-02 15:00:28 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import React, { Component } from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import { Route, Routes } from 'react-router-dom'; 
-
-import FooterComponent from './components/FooterComponent';
-import HeaderComponent from './components/HeaderComponent';
-import AccountComponent from './components/AccountComponent';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ListCustomerComponent from "./components/ListCustomersComponent";
 import CreateCustomerComponent from "./components/CreateCustomerComponent";
-import DepositsComponent from "./components/DepositsComponent";
-
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 
 function App() {
   return (
     <div>
       <div className="container">
-        <HeaderComponent />
         <Routes>
-          <Route exact path="/" element={<DepositsComponent />} />
+          <Route exact path="/" element={<ListCustomerComponent />} />
           <Route exact path="/customers" element={<ListCustomerComponent />} />
-          <Route exact path="/list-customers" element={<ListCustomerComponent />} />
-          <Route path = '/accounts' element = { <AccountComponent/>} />
           <Route path="/create-customer" element={<CreateCustomerComponent />} />
           <Route path="/edit-customer/:id" element={<CreateCustomerComponent />} />
         </Routes>
-       <FooterComponent/>
       </div>
     </div>
   );
