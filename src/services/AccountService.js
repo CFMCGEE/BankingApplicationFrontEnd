2604ebadf3d3b161e06bb21f030d9abe494c77c0
CFMCGEE <funchbusiness@gmail.com>
2021-12-01 23:56:41 -0500

    Committing changes

--- src/services/AccountService.js
+++ src/services/AccountService.js
@@ -5,8 +5,13 @@ const ACCOUNT_BASE_API = "http://localhost:8080/accounts";
 class AccountService { 
 
     createAccount(account) { 
-        return axios.post(ACCOUNT_BASE_API , account)
+        return axios.post(ACCOUNT_BASE_API, account)
     }
+
+    getAllAccounts() { 
+        return axios.get(ACCOUNT_BASE_API)
+    }
+    
 
 }
 
