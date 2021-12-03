b3d309cd383476456f3b0594dcc95bb11c023731
Diego Guzman <deeaygo98@gmail.com>
2021-12-03 17:25:08 -0500

    bill paths

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ const App = () => {
 
           <Route exact path="/accounts" element={<DisplayAccountsComponent />} />
           <Route exact path="/customers" element={<DisplayCustomersComponent />} />
+          <Route exact path="/view-bills/:id" element={<DisplayBillsComponent />} />
 
           <Route exact path="/update-account/:id" element={<CreateAccountComponent />} />
           <Route exact path="/update-customer/:id" element={<CreateCustomerComponent />} />
