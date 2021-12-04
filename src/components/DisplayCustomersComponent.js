b3b6da4bdf8ab4fb7381fe260bb3c5c456f94d41
Boris <borisgerard333@gmail.com>
2021-12-04 18:53:58 -0500

    Added CSV

--- src/components/DisplayCustomersComponent.js
+++ src/components/DisplayCustomersComponent.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import CustomerService from "../services/CustomerService";
-import { InputGroup, FormControl } from 'react-bootstrap';
+import { InputGroup, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
 
 const DisplayCustomerComponent = () => {
   //function to change the state value
@@ -12,6 +13,12 @@ const DisplayCustomerComponent = () => {
       last_Name: "",
     },
   ]);
+
+  const csvFile = {
+    id: "",
+    first_Name: "",
+    last_Name: "",
+  };
 
   const [q, setQ] = useState("");
 
@@ -35,21 +42,29 @@ const DisplayCustomerComponent = () => {
     <div className="container">
       <br />
       <h2 className="text-center">Customers</h2>
-      <br /> 
+      <br />
       {/* <Link to="/create-customer" className="btn btn-primary mb-2">
         Create Customer
       </Link> */}
-    <InputGroup className="mb-3">
-    <FormControl
-        type="text"
-        className="form-control"
-        placeholder="Search customers"
-        onChange={(e) => {
-          setQ(e.target.value);
-        }}
+      <ReactHTMLTableToExcel
+        id="test-table-xls-button"
+        className="download-table-xls-button"
+        table="customer-table"
+        filename="customer-list"
+        sheet="tablexls"
+        buttonText="Download as XLS"
+      />
+      <InputGroup className="mb-3">
+        <FormControl
+          type="text"
+          className="form-control"
+          placeholder="Search customers"
+          onChange={(e) => {
+            setQ(e.target.value);
+          }}
         />
-     </InputGroup>
-      <br /> 
+      </InputGroup>
+      <br />
       <table className="table table-bordered table-striped">
         <thead>
           <th>Customer ID</th>
@@ -59,7 +74,8 @@ const DisplayCustomerComponent = () => {
         </thead>
         <tbody>
           {/*eslint-disable-next-line*/}
-          {customer.filter((customer) => {
+          {customer
+            .filter((customer) => {
               if (q === "") {
                 return customer;
               } else if (customer.first_Name.toLowerCase().includes(q.toLowerCase()) || customer.last_Name.toLowerCase().includes(q.toLowerCase())) {
@@ -72,12 +88,34 @@ const DisplayCustomerComponent = () => {
                 <td class="bg-info">{customer.first_Name}</td>
                 <td class="bg-info">{customer.last_Name}</td>
                 <td>
-                  <Link to={`/update-customer/${customer.id}`} className="btn btn-primary"> Update </Link> 
+                  <Link to={`/update-customer/${customer.id}`} className="btn btn-primary">
+                    {" "}
+                    Update{" "}
+                  </Link>
                 </td>
               </tr>
             ))}
         </tbody>
       </table>
+
+      <div>
+        <table className="table table-bordered table-striped" id="customer-table" style={{ display: "none" }}>
+          <thead>
+            <th>Customer ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </thead>
+          <tbody>
+            {customer.map((customer) => (
+              <tr key={customer.id}>
+                <td class="bg-success">{customer.id}</td>
+                <td class="bg-info">{customer.first_Name}</td>
+                <td class="bg-info">{customer.last_Name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
